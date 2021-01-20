15b730b0294b648c971caa4da4f2c77cb07429a3
Partho P. Das <parthopdas@live.com>
2021-01-20 23:09:20 +1000

    Step 1 of 3: Just move code

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -10,4 +10,19 @@ declare module '@pathnirvanafoundation/pali-script-converter' {
     constructor()
     get name()
   }
+
+  export class TextProcessor {
+    static basicConvert(text: string, script: string): string
+    static basicConvertFrom(text: string, script: string): string
+    static beautify(text: string, script: string, rendType = ''): string
+    static convert(text: string, script: string): string
+    static convertFrom(text: string, script: string): string
+    static convertFromMixed(mixedText: string): string
+  }
+
+  export const Script: any
+
+  export const paliScriptInfo: Map
+
+  export const getScriptForCode: (charCode: number) => string
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import Obj1 from './obj1'
 
 export default { Obj1, Obj2 }
 
+export * from './pali-script'
+
 console.log('Hello world from the bundle!')
